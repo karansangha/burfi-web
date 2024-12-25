cec9ec505b43fddda93e4184a24aa92c0f3cc3c5
Karan Sangha <iamkaransangha@gmail.com>
2024-12-25 09:41:38 -0500

    Update dogVisionFilter.js

--- dogVisionFilter.js
+++ dogVisionFilter.js
@@ -1,27 +1,28 @@
+// Select DOM elements
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d', { willReadFrequently: true });
 const switchCameraButton = document.getElementById('switch-camera');
 const capturePhotoButton = document.getElementById('capture-photo');
 
-// Fix for iOS Safari from https://leemartin.dev/hello-webrtc-on-safari-11-e8bcb5335295
-video.setAttribute('autoplay', '');
-video.setAttribute('muted', '');
-video.setAttribute('playsinline', '')
+// Fix for iOS Safari
+enableIOSVideoAttributes(video);
 
 let currentStream;
 let useFrontCamera = false;
 
-/*
-Source - https://mk.bcgsc.ca/colorblind/math.mhtml#projecthome
-Based on Martin Krzywinski’s incredible work, this is the RGB transformation matrix 
-of a deuteranopia vision
-*/
-const matrix = [
+// Deuteranopia RGB transformation matrix
+const colorMatrix = [
     [0.33066007, 0.66933993, 0],
     [0.33066007, 0.66933993, 0],
     [-0.02785538, 0.02785538, 1]
 ];
+
+function enableIOSVideoAttributes(videoElement) {
+    videoElement.setAttribute('autoplay', '');
+    videoElement.setAttribute('muted', '');
+    videoElement.setAttribute('playsinline', '');
+}
 
 function startCamera() {
     if (currentStream) {
@@ -29,93 +30,88 @@ function startCamera() {
     }
 
     const constraints = {
-        video: {
-            facingMode: useFrontCamera ? 'user' : 'environment'
-        }
+        video: { facingMode: useFrontCamera ? 'user' : 'environment' }
     };
 
-    if (localStorage.getItem('cameraPermissionGranted')) {
-        startCamera();
-    } else {
-        navigator.mediaDevices.getUserMedia(constraints)
-            .then(stream => {
-                currentStream = stream;
-                video.srcObject = stream;
-                video.play();
-            })
-            .catch(err => {
-                console.error("Error accessing camera: ", err);
-            });
-    }
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(stream => {
+            currentStream = stream;
+            video.srcObject = stream;
+            video.play();
+        })
+        .catch(err => {
+            console.error("Error accessing camera: ", err);
+        });
 }
 
-function applyDogVisionFilter() {
+function applyFilterToImageData(imageData, transformationMatrix) {
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+        const r = data[i];
+        const g = data[i + 1];
+        const b = data[i + 2];
+
+        data[i] = transformationMatrix[0][0] * r + transformationMatrix[0][1] * g + transformationMatrix[0][2] * b;
+        data[i + 1] = transformationMatrix[1][0] * r + transformationMatrix[1][1] * g + transformationMatrix[1][2] * b;
+        data[i + 2] = transformationMatrix[2][0] * r + transformationMatrix[2][1] * g + transformationMatrix[2][2] * b;
+    }
+
+    return imageData;
+}
+
+function drawFilteredFrame() {
     if (video.videoWidth > 0 && video.videoHeight > 0) {
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        let data = imageData.data;
-
-        for (let i = 0; i < data.length; i += 4) {
-            const r = data[i];
-            const g = data[i + 1];
-            const b = data[i + 2];
-
-            data[i] = matrix[0][0] * r + matrix[0][1] * g + matrix[0][2] * b;
-            data[i + 1] = matrix[1][0] * r + matrix[1][1] * g + matrix[1][2] * b;
-            data[i + 2] = matrix[2][0] * r + matrix[2][1] * g + matrix[2][2] * b;
-        }
-
-        ctx.putImageData(imageData, 0, 0);
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const filteredData = applyFilterToImageData(imageData, colorMatrix);
+        ctx.putImageData(filteredData, 0, 0);
     }
-    requestAnimationFrame(applyDogVisionFilter);
+    requestAnimationFrame(drawFilteredFrame);
+}
+
+function capturePhoto() {
+    if (video.videoWidth > 0 && video.videoHeight > 0) {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const filteredData = applyFilterToImageData(imageData, colorMatrix);
+        ctx.putImageData(filteredData, 0, 0);
+
+        const dataURL = canvas.toDataURL('image/png');
+        downloadImage(dataURL, 'photo.png');
+    }
+}
+
+function downloadImage(dataURL, filename) {
+    const link = document.createElement('a');
+    link.href = dataURL;
+    link.download = filename;
+    link.click();
+}
+
+function handleSwitchCamera() {
+    useFrontCamera = !useFrontCamera;
+    startCamera();
 }
 
 video.addEventListener('play', () => {
-    function checkVideoReady() {
+    function waitForVideoReady() {
         if (video.videoWidth > 0 && video.videoHeight > 0) {
-            applyDogVisionFilter();
+            drawFilteredFrame();
         } else {
-            requestAnimationFrame(checkVideoReady);
+            requestAnimationFrame(waitForVideoReady);
         }
     }
-    checkVideoReady();
+    waitForVideoReady();
 });
 
-switchCameraButton.addEventListener('click', () => {
-    useFrontCamera = !useFrontCamera;
-    startCamera();
-});
-
-capturePhotoButton.addEventListener('click', () => {
-    if (video.videoWidth > 0 && video.videoHeight > 0) {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-        let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        let data = imageData.data;
-
-        for (let i = 0; i < data.length; i += 4) {
-            const r = data[i];
-            const g = data[i + 1];
-            const b = data[i + 2];
-
-            data[i] = matrix[0][0] * r + matrix[0][1] * g + matrix[0][2] * b;
-            data[i + 1] = matrix[1][0] * r + matrix[1][1] * g + matrix[1][2] * b;
-            data[i + 2] = matrix[2][0] * r + matrix[2][1] * g + matrix[2][2] * b;
-        }
-
-        ctx.putImageData(imageData, 0, 0);
-
-        const dataURL = canvas.toDataURL('image/png');
-        const link = document.createElement('a');
-        link.href = dataURL;
-        link.download = 'photo.png';
-        link.click();
-    }
-});
+switchCameraButton.addEventListener('click', handleSwitchCamera);
+capturePhotoButton.addEventListener('click', capturePhoto);
 
 startCamera();
